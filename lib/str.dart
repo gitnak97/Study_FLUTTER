f5f397341e13e1e72f3b9bb9c92f6bd4092f9a5a
nakhoon <nak970129@naver.com>
2025-12-08 09:59:37 +0900

    GIT ADD

---
+++ lib/str.dart
@@ -0,0 +1,6 @@
+void main() {
+  var name = 'nico';
+  var age = 10;
+  var greeting = 'Hello, $name! i am ${age + 2} years old.';
+  print(greeting);
+}
--- lib/test.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  var giveMeFive = true;
-  var numbers = [1, 2, 3, 4, if (giveMeFive) 5];
-  print(numbers);
-}
-
-void unusedFunction(Object c) {}
