f144646a7df845acb5260a24d1cb56680b72ea8f
nakhoon <nak970129@naver.com>
2025-12-09 09:06:27 +0900

    2025 12 09 공부

--- lib/collection.dart
+++ lib/collection.dart
@@ -1,5 +1,9 @@
 void main() {
   var oldFriends = ['nico', 'lynn'];
-  var newFriends = ['sarah', 'jack', for (var friend in oldFriends) '$friend'];
+  var newFriends = [
+    'sarah',
+    'jack',
+    for (var friend in oldFriends) " VS $friend",
+  ];
   print(newFriends);
 }
